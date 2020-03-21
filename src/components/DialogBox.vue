3c1faf830491eab952885bcd61c9202ece099265
tetianabilyayeva <tbilyayeva@yahoo.com>
2020-03-21 06:40:52 -0400

    finished project
    finished

--- src/components/DialogBox.vue
+++ src/components/DialogBox.vue
@@ -1,94 +1,99 @@
 <template>
   <div class="message-box dialog-mask" @click="$close">
+
     <div class="dialog-content">
+     <div class="dialog-close">
+     <button class="el-button" type="text"  @click="$close">Close</button>
+     </div>
       <div class="dialog-body">
-        <img v-bind:alt="content.ItemName" v-bind:src="content.PhotoName"/>
-        <h2>{{ content.ItemName }}</h2>
-        <span>{{content.ItemID}}</span>
+
+        <img class="block-image" v-bind:alt="content.ItemName" v-bind:src="content.PhotoName"/>
+        <h2>{{ content.ItemName }} (ID:{{content.ItemID}})</h2>
+        
+        
+        <p>Dimensions: {{content.Dimensions}}</p>
        <div v-if="content.Description">{{content.Description}}</div>
        <div v-else>Description coming soon.</div>
-       <span>{{content.Dimensions}}</span>
-       <span>{{content.BasePrice | currency}}</span>
+       <span class="block-price">{{content.BasePrice | currency}}</span>
       </div>
-      <footer>
-        <button class="el-button" type="text"  @click="$close">Close</button>
-      </footer>
     </div>
   </div>
 </template>
 
 
 <style scoped lang="scss">
-.dialog-mask {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-align: center;
-    align-items: center;
-    -ms-flex-pack: center;
-    justify-content: center;
-    background-color: rgba(0,0,0,.33);
+
+/*Dialog styles*/
+.dialog {
+    &-mask {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-align: center;
+        align-items: center;
+        -ms-flex-pack: center;
+        justify-content: center;
+        background-color: rgba(0,0,0,.33); 
+    }
+
+    &-content {
+        margin: 0 20px;
+        padding: 20px 30px;
+        border: 1px solid #ebeef5;
+        border-radius: 4px;
+        background-color: #fff;
+        box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
+        color: #303133;
+        max-width: 500px;
+    }
+
+    &-close {
+        text-align: right;
+    }
 }
 
-.message-box .dialog-content {
-    max-width: 400px;
-    text-align: center;
-    font-size: 16px;
-}
-
-.dialog-content {
-    margin: 0 20px;
-    padding: 20px 30px;
-    border: 1px solid #ebeef5;
-    border-radius: 4px;
-    background-color: #fff;
-    box-shadow: 0 2px 12px 0 rgba(0,0,0,.1);
-    color: #303133;
-}
-
-.dialog-content footer {
-    display: -ms-flexbox;
-    display: flex;
-    margin-top: 24px;
-}
-.dialog-content .el-button {
-    -ms-flex: 1;
-    flex: 1;
-    -webkit-appearance: none;
-    -moz-appearance: none;
-    appearance: none;
-}
 
 .el-button {
-    display: inline-block;
-    line-height: 1;
-    white-space: nowrap;
     cursor: pointer;
-    background: #3a8ee6;
-    border-color: #3a8ee6;
-    color: #fff;
-    -webkit-appearance: none;
-    text-align: center;
-    box-sizing: border-box;
-    outline: 0;
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
     margin: 0;
     transition: .1s;
     padding: 12px 20px;
     font-size: 14px;
     border-radius: 4px;
     border-style: none;
+
 }
 
 .el-button:focus, .el-button:hover {
-    color: #409eff;
-    border-color: #c6e2ff;
-    background-color: #ecf5ff;
+    background: #b8cacf;
+    border-color: #3a8ee6;
+    color: #fff;
 }
-img {
-  max-width: 300px;
+
+/*End Dialog styles*/
+
+.block {
+    &-image {
+        max-width: 250px;
+        @media only screen and (max-width: 767px) {
+          max-width: 120px;
+        }
+    }
+    &-price {
+        padding: 10px;
+        border: 1px solid #56a1b3;
+        background: #119bba;
+        color: #fff;
+        font-weight: 600;
+        display: block;
+        margin: 10px 0;
+    }
 }
 </style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,7 @@
   <div>
   	
     <p>Hello! This simple application was developed as part of technical assignment for RepZio by Tetiana Bilyayeva.</p>
-    <p>The time for development was limited to 24 hours. I started on 3/20 around 10:00 am and finished by 3/21 6:00am</p>
+    <p>The time for development was limited to 24 hours. I started on 3/20 around 10:00 am and finished by 3/21 8:00am</p>
     <p>Please contact me via <a href="mailto:tbilyayeva@gmail.com" target="_top">tbilyayeva@gmail.com</a> or by my cell phone: <a href="tel:9045256018">904-525-6018</a> to provide your feedback about my work. I appreciate your honest opinion and will be happy to discuss what could be done better. </p>
 
     <p>Have a great day! I look forward to hearing from you soon!</p>
